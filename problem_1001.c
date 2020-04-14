3fb7e705ec086233a05a77a4da950dfd80bddbbc
WandersonGomes <dersom100@gmail.com>
2020-04-14 20:06:46 -0300

    Problem 1001

---
+++ problem_1001.c
@@ -0,0 +1,16 @@
+//PROBLEM 1001
+//link = https://www.urionlinejudge.com.br/judge/pt/problems/view/1001
+
+#include <stdio.h>
+
+int main() {
+    int numero1 = 0, numero2 = 0, tmp_soma = 0;
+    scanf("%d", &numero1);
+    scanf("%d", &numero2);
+
+    tmp_soma = numero1 + numero2;
+
+    printf("X = %d\n", tmp_soma);
+
+    return 0;
+}
