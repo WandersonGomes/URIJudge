01220a1bf7d09c741ee066a0a6cfd3aaee83c4a0
WandersonGomes <dersom100@gmail.com>
2020-04-15 15:40:26 -0300

    Problem 1003

---
+++ problem_1003.c
@@ -0,0 +1,12 @@
+//PROBLEM 1003
+//link = https://www.urionlinejudge.com.br/judge/pt/problems/view/1003
+
+int main(){
+    int numero1 = 0, numero2 = 0;
+
+    scanf("%d %d", &numero1, &numero2);
+
+    printf("SOMA = %d\n", (numero1 + numero2));
+
+    return 0;
+}
