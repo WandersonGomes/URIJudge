506d7da757931f70bdd83079bc19d3b5d71e3ac8
WandersonGomes <dersom100@gmail.com>
2020-04-15 15:49:32 -0300

    Problem 1004

---
+++ problem_1004.c
@@ -0,0 +1,13 @@
+//PROBLEM 1004
+//link = https://www.urionlinejudge.com.br/judge/pt/problems/view/1004
+#include <stdio.h>
+
+int main() {
+    int numero1 = 0, numero2 = 0;
+    
+    scanf("%d %d", &numero1, &numero2);
+
+    printf("PROD = %d\n", (numero1 * numero2));
+
+    return 0;
+}
