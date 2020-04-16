f39cfbfb0754b9f56c8ac383287505a3c8d1f8b0
WandersonGomes <dersom100@gmail.com>
2020-04-16 17:03:45 -0300

    Problem 2963

---
+++ problem_2963.c
@@ -0,0 +1,32 @@
+//PROBLEM 2963
+//link = https://www.urionlinejudge.com.br/judge/pt/problems/view/2963
+#include <stdio.h>
+
+#define POSICAO_CARLOS 1
+
+int main() {
+    int quantidade_participantes = 0, posicao_vencedora = 1;
+    long int numero_votos = 0, votos_carlos = 0;
+
+    scanf("%d", &quantidade_participantes);
+    
+    scanf("%d", &numero_votos);
+    votos_carlos = numero_votos;
+
+    int i = 1;
+    for (; i < quantidade_participantes; i++) {
+        scanf("%d", &numero_votos);
+        if (numero_votos > votos_carlos) {
+            posicao_vencedora = 0; //carlos nao venceu eh apenas o que desejamos saber. O loop continua por causa da leitura dos votos
+        }
+    }
+
+    //analisa o resultado
+    if (posicao_vencedora) {
+        printf("S\n");
+    } else {
+        printf("N\n");
+    }
+
+    return 0;
+}
