a2618472d6aad95985d45410750f63055e49f8da
WandersonGomes <dersom100@gmail.com>
2020-05-03 20:01:19 -0300

    Problem 1759

---
+++ problem_1759.py
@@ -0,0 +1,9 @@
+#PROBLEM 1759
+#link = https://www.urionlinejudge.com.br/judge/pt/runs/code/18050379
+#PYTHON 3
+
+quantidadeHo = int(input())
+for i in range(0, quantidadeHo-1):
+    print("Ho ",end="")
+
+print("Ho!")
