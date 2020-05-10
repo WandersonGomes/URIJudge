895a7fdea74190fa3a4fa8f8a800d59801511fd7
WandersonGomes <dersom100@gmail.com>
2020-05-10 12:22:16 -0300

    Problem 1957

---
+++ problem_1957.c
@@ -0,0 +1,10 @@
+//PROBLEM 1957
+//link = https://www.urionlinejudge.com.br/judge/pt/runs/code/17163068
+#include <stdio.h>
+
+int main() {
+    long int valor = 0;
+    scanf("%ld", &valor);
+    printf("%0lX\n", valor);
+    return 0;
+}
