ad3257e6bf8a4d15aa53c5b5063334224ab1645f
WandersonGomes <dersom100@gmail.com>
2020-05-18 16:31:16 -0300

    Problem 1153

---
+++ problem_1153.c
@@ -0,0 +1,28 @@
+//PROBLEM 1153
+//link = https://www.urionlinejudge.com.br/judge/pt/problems/view/1153
+#include <stdio.h>
+
+//FUNCAO QUE RETORNA O VALOR DO FATORIAL CALCULO SIMPLES
+long int fatorial(int numero) {
+    if (numero == 1) {
+        return 1;
+    } else {
+        int produto = 1;
+        int i = 1;
+        for (; i <= numero; i++) {
+            produto = produto * i;
+        }
+
+        return produto;
+    }
+    return 0;
+}
+
+//PROGRAMA PRINCIPAL
+int main(){
+    int numero = 0;
+    scanf("%d", &numero);
+
+    printf("%ld\n", fatorial(numero));
+    return 0;
+}
