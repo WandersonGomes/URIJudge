88872881ddbaa14bf175cf9f2d7c9420f174d127
WandersonGomes <dersom100@gmail.com>
2020-05-21 19:14:13 -0300

    Problem 1198

---
+++ problem_1198.c
@@ -0,0 +1,23 @@
+//PROBLEM 1198
+//link = 
+#include <stdio.h>
+
+//FUNCAO QUE RETORNA O VALOR ABSOLUTO
+long long int meu_abs(long long int numero) {
+    if (numero < 0) {
+        return (-1)*numero;
+    }
+    return numero;
+}
+
+
+//PROGRAMA PRINCIPAL
+int main() {
+    long long int hashmat = 0, oponente = 0;
+
+    while (scanf("%lld %lld", &hashmat, &oponente) != EOF){
+        printf("%lld\n", meu_abs(hashmat - oponente));
+    }
+
+    return 0;
+}
