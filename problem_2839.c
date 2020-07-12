d4c2e9d114fef4a7f9c0438bc7fb126b4a366e60
WandersonGomes <dersom100@gmail.com>
2020-07-11 23:31:40 -0300

    Problem 2839

---
+++ problem_2839.c
@@ -0,0 +1,15 @@
+//PROBLEM 2839
+//link = https://www.urionlinejudge.com.br/judge/pt/problems/view/2839
+//Obs.:
+//Video aula sobre o assunto:
+//https://www.youtube.com/watch?v=rtaA-_-0bEw
+#include <stdio.h>
+
+int main() {
+    long long int numero_pares = 0;
+    
+    scanf("%lld", &numero_pares);
+    printf("%lld\n", numero_pares + 1);
+    
+    return 0;
+}
