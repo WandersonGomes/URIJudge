692644022f1e53603af740330ef3b2985a67cc0f
WandersonGomes <dersom100@gmail.com>
2020-07-31 22:55:20 -0300

    Problem 1016

---
+++ problem_1016.c
@@ -0,0 +1,13 @@
+//PROBLEM 1016
+//link = https://www.urionlinejudge.com.br/judge/pt/problems/view/1016
+#include <stdio.h>
+
+int main(){
+    int distancia = 0;
+
+    scanf("%d", &distancia);
+
+    printf("%d minutos\n", (distancia * 2));
+
+    return 0;
+}
