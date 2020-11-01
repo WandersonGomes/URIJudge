ef57e238387db2813d00fca7142fc21bb4976bae
WandersonGomes <dersom100@gmail.com>
2020-10-31 21:39:43 -0300

    Problem 3157

---
+++ problem_3157.c
@@ -0,0 +1,11 @@
+//PROBLEM 3157
+//link = https://www.urionlinejudge.com.br/judge/pt/problems/view/3157
+#include <stdio.h>
+
+int main() {
+   scanf("%*s");
+
+   printf("Nao se refuta ciencia com opiniao\n");
+   
+   return 0;
+}
