b0bb2fa44c3b2bbc2ac0c4169379d309ebf0718a
WandersonGomes <dersom100@gmail.com>
2021-08-05 12:05:41 -0300

    Problem 3299

---
+++ problem_3299.c
@@ -0,0 +1,29 @@
+//PROBLEM 3299
+//link = https://www.urionlinejudge.com.br/judge/pt/problems/view/3299
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    //dados
+    char numero[18];
+    int tamanho = 0, i = 0;
+
+    //leitura
+    scanf("%s", numero);
+
+    //calculo
+    tamanho = strlen(numero);
+    for (i = 0; i < (tamanho - 1); i++) {
+        if (numero[i] == '1') {
+            if (numero[i+1] == '3') {
+                printf("%s es de Mala Suerte\n", numero);
+                break;
+            }
+        }
+    }
+
+    if (i == (tamanho - 1))
+        printf("%s NO es de Mala Suerte\n", numero);
+
+    return 0;
+}
