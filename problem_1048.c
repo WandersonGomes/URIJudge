4ce86896941f5c9ddb92af0d8271315661cf6fef
WandersonGomes <dersom100@gmail.com>
2022-12-22 23:07:56 -0300

    Problem 1048

---
+++ problem_1048.c
@@ -0,0 +1,38 @@
+//PROBLEM 1048
+//link = 
+#include <stdio.h>
+
+//FUNCOES
+void imprimirDados(double salario, int porcentagem) {
+    printf("Novo salario: %.2lf\n"
+            "Reajuste ganho: %.2lf\n"
+            "Em percentual: %d %%\n",
+            (salario + (salario * porcentagem/100.00)),
+            (salario * porcentagem/100.00),
+            porcentagem
+            );
+}
+
+//PROGRAM PRINCIPAL
+int main() {
+    //dados
+    double salario;
+
+    //leitura
+    scanf("%lf", &salario);
+
+    //processamento - saida
+    if (salario >= 0 && salario <= 400.00) {
+        imprimirDados(salario, 15);
+    } else if (salario <= 800.00) {
+        imprimirDados(salario, 12);
+    } else if (salario <= 1200.00) {
+        imprimirDados(salario, 10);
+    } else if (salario <= 2000.00) {
+        imprimirDados(salario, 7);
+    } else {
+        imprimirDados(salario, 4);
+    }
+
+    return 0;
+}
