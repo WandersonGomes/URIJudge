1ef9140791b6dfdbcfeb1e4bfd5ce74a93dd8f7e
WandersonGomes <dersom100@gmail.com>
2022-12-23 22:04:33 -0300

    Problem 1059

--- problem_1059.c
+++ problem_1059.c
@@ -4,8 +4,9 @@
 
 int main() {
     int numero = 0;
-    for (numero = 2; numero <= 100; numero = numero + 2) {
+
+    for (numero = 2; numero <= 100; numero += 2) 
         printf("%d\n", numero);
-    }
+    
     return 0;
 }
